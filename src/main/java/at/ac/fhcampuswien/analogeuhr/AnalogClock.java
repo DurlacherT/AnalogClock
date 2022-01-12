0e59f63a1decc117fa4ecf84800adeda193f973d
Thomas Durlacher <thomas.durlacher@gmail.com>
2022-01-12 10:35:16 +0100

    push

--- src/main/java/at/ac/fhcampuswien/analogeuhr/AnalogClock.java
+++ src/main/java/at/ac/fhcampuswien/analogeuhr/AnalogClock.java
@@ -8,8 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.Scanner;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -25,7 +23,7 @@ public class AnalogClock extends JPanel implements Runnable
     int lastxh = 0;
     int lastyh = 0;
     Thread thread;
-    SimpleDateFormat formattedDate = new SimpleDateFormat("s", Locale.getDefault());
+    SimpleDateFormat formattedDate = new SimpleDateFormat();
     Date date;
 
     public static void setBackground(int background) {
@@ -42,19 +40,19 @@ public class AnalogClock extends JPanel implements Runnable
             try {
                 img = ImageIO.read(new File("image0.jpg"));
             } catch (
-                    IOException ignored) {
+                    IOException e) {
             }
         } else if (design==2) {
             try {
                 img = ImageIO.read(new File("image1.jpg"));
             } catch (
-                    IOException ignored) {
+                    IOException e) {
             }
         } else if (design==3) {
             try {
                 img = ImageIO.read(new File("image2.jpg"));
             } catch (
-                    IOException ignored) {
+                    IOException e) {
             }
         } else {
             try {
@@ -64,12 +62,9 @@ public class AnalogClock extends JPanel implements Runnable
             }
         }
 
-        //innere Farbe (Uhr)
-        g.setColor(Color.white);
+        g.drawImage(img, 0, 0, null); //Eingabe die Hintergrund auswählt
 
-            g.drawImage(img, 0, 0, null); //Eingabe die Hintergrund auswählt
-
-        //Farbe Uhrzeit (12, 3, 6, 9)
+        //Fügt römische Ziffern hinzu
         g.setColor(Color.black);
         g.drawString("XII", xposition - 20, yposition - 220);
         g.drawString("III", xposition + 215, yposition);
@@ -91,9 +86,9 @@ public class AnalogClock extends JPanel implements Runnable
         int second;
         int minute;
         int hour;
-        Graphics2D g = (Graphics2D) g1; // 2D wird für 2D Zeiger gebraucht
+        Graphics2D g = (Graphics2D) g1; // 2D Grphics Objekt wird für 2D Zeiger benötigt
 
-        //Kreis (Uhr)
+        //Methode drawBackground zeichnet Hintergrund
         drawBackground(g);
 
         //Datum
@@ -107,37 +102,16 @@ public class AnalogClock extends JPanel implements Runnable
         hour = Integer.parseInt(formattedDate.format(date));
 
         //x+y center = Beginn des Zeigers
-        //120 = ZeigerLänge
         xsecond = (int)(Math.cos(second * Math.PI / 30 - Math.PI / 2) * 220 + xposition);
         ysecond = (int)(Math.sin(second * Math.PI / 30 - Math.PI / 2) * 220 + yposition);
 
         //x+y center = Beginn des Zeigers
-        //100 = ZeigerLänge
         xminute = (int)(Math.cos(minute * Math.PI / 30 - Math.PI / 2) * 200 + xposition);
         yminute = (int)(Math.sin(minute * Math.PI / 30 - Math.PI / 2) * 200 + yposition);
 
         //x+y center = Beginn des Zeigers
-        //80 = ZeigerLänge
         xhour = (int)(Math.cos((hour * 30 + minute / 2.0) * Math.PI / 180 - Math.PI / 2) * 180 + xposition);
         yhour = (int)(Math.sin((hour * 30 + minute / 2.0) * Math.PI / 180 - Math.PI / 2) * 180 + yposition);
-
-        //aktuelle Uhrzeit darstellen
-        if (xsecond != lastxs || ysecond != lastys)
-        {
-            g.drawLine(xposition, yposition, lastxs, lastys);
-        }
-
-        if (xminute != lastxm || yminute != lastym)
-        {
-            g.drawLine(xposition, yposition - 1, lastxm, lastym);
-            g.drawLine(xposition - 1, yposition, lastxm, lastym);
-        }
-
-        if (xhour != lastxh || yhour != lastyh)
-        {
-            g.drawLine(xposition, yposition - 1, lastxh, lastyh);
-            g.drawLine(xposition - 1, yposition, lastxh, lastyh);
-        }
 
         g.setStroke(new BasicStroke(6)); // Breite Zeiger
         //SekundenZeiger Farbe
@@ -194,15 +168,12 @@ public class AnalogClock extends JPanel implements Runnable
 
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        Boolean error = true;
-
-        //Objekt
+        //Erzeugt Objekt der Klasse AnalogClock
         AnalogClock clock = new AnalogClock();
 
-        JFrame f=new JFrame("Button window");
-        final JTextField newfe=new JTextField();
-        newfe.setBounds(50,25, 150,20);
+        JFrame buttonWindow = new JFrame("Button window");
+        final JTextField textfield = new JTextField();
+        textfield.setBounds(50,25, 150,20);
 
         JButton button3=new JButton("Click here for design 3");
         JButton button2=new JButton("Click here for design 2");
@@ -216,59 +187,59 @@ public class AnalogClock extends JPanel implements Runnable
 
         button1.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                newfe.setText("Design 1");
+                textfield.setText("Design 1");
                 setBackground(1);
             }
         });
         button2.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                newfe.setText("Design 2");
+                textfield.setText("Design 2");
                 setBackground(2);
             }
         });
         button3.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                newfe.setText("Design 3");
+                textfield.setText("Design 3");
                 setBackground(3);
             }
         });
         button4.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                newfe.setText("Exit");
+                textfield.setText("Exit");
                 System.exit(0);
             }
         });
 
-        f.add(button3);
-        f.add(button2);
-        f.add(button1);
-        f.add(button4);
+        buttonWindow.add(button1);
+        buttonWindow.add(button2);
+        buttonWindow.add(button3);
+        buttonWindow.add(button4);
 
-        f.add(newfe);
-        f.setBounds(600,100,300,300);
-        f.setLayout(null);
-        f.setVisible(true);
+        buttonWindow.add(textfield);
+        buttonWindow.setBounds(600,100,300,300);
+        buttonWindow.setLayout(null);
+        buttonWindow.setVisible(true);
 
         //Fenster Pop-Up
-        JFrame window = new JFrame();
+        JFrame windowClock = new JFrame();
 
         //Hintergrundfarbe
         Color c = new Color(255, 255, 255);
 
         //Hintergrundfarbe im Fenster implementieren
-        window.setBackground(c);
+        windowClock.setBackground(c);
 
         //Pop-up-Fenster schließen
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windowClock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Länge+Breite vom Fenster
-        window.setBounds(0, 0, 600, 600);
+        windowClock.setBounds(0, 0, 600, 600);
 
         //Objektimport im Fenster
-        window.getContentPane().add(clock);
+        windowClock.getContentPane().add(clock);
 
         //Sichtbarkeit
-        window.setVisible(true);
+        windowClock.setVisible(true);
 
         //Start bzw. Anfang
         clock.start();
