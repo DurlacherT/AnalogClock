25ccc98d0d294f00e0e0a4d0536715f5117044c0
Thomas Durlacher <thomas.durlacher@gmail.com>
2022-01-22 19:55:53 +0100

    Peer review Vorschläge eingearbeitet

--- src/main/java/at/ac/fhcampuswien/analogeuhr/AnalogClock.java
+++ src/main/java/at/ac/fhcampuswien/analogeuhr/AnalogClock.java
@@ -1,8 +1,6 @@
 package at.ac.fhcampuswien.analogeuhr;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -29,24 +27,24 @@ public class AnalogClock extends JPanel implements Runnable
         switch (design) {
             case 1:
                 try {
-                    img = ImageIO.read(new File("image0.jpg"));
+                    img = ImageIO.read(new File("src/main/resources/image0.jpg"));
                 } catch (IOException e) {
                 } break;
             case 2: {
                 try {
-                    img = ImageIO.read(new File("image1.jpg"));
+                    img = ImageIO.read(new File("src/main/resources/image1.jpg"));
                 } catch (IOException e) {
                 } break;
             }
             case 3: {
                 try {
-                    img = ImageIO.read(new File("image2.jpg"));
+                    img = ImageIO.read(new File("src/main/resources/image2.jpg"));
                 } catch (IOException e) {
                 } break;
             }
                 default:
                     try {
-                    img = ImageIO.read(new File("image3.jpg"));
+                    img = ImageIO.read(new File("src/main/resources/image3.jpg"));
                 } catch (IOException e) {
                 }
             }
@@ -55,6 +53,9 @@ public class AnalogClock extends JPanel implements Runnable
 
         //Adds Roman numerals to the background.
         g.setColor(Color.black);
+        Font currentFont = g.getFont();
+        Font newFont = currentFont.deriveFont(currentFont.getSize() * 2.4F);
+        g.setFont(newFont);
         g.drawString("XII", XCLOCKPOSITION - 20, YCLOCKPOSITION - 220);
         g.drawString("III", XCLOCKPOSITION + 215, YCLOCKPOSITION);
         g.drawString("VI", XCLOCKPOSITION - 10, YCLOCKPOSITION + 245);
@@ -146,65 +147,69 @@ public class AnalogClock extends JPanel implements Runnable
         AnalogClock.design = background;
     }
 
-
-    public static void main(String[] args)
-    {
-        //Create object of the class AnalogClock
-        AnalogClock clock = new AnalogClock();
-
+    /*
+     * Create a window with buttons
+     */
+    public void createButtons(){
         //The first JFrame object is used for the button window.
         JFrame buttonWindow = new JFrame("Button window");
         final JTextField textfield = new JTextField();
         textfield.setBounds(50,25, 150,20);
+        textfield.setEditable(false);
 
         //Set Button properties
+        JButton button0=new JButton("No background");
         JButton button3=new JButton("Click here for design 3");
         JButton button2=new JButton("Click here for design 2");
         JButton button1=new JButton("Click here for design 1");
         JButton button4=new JButton("Stop");
 
-        button1.setBounds(50,50,200,30);
-        button2.setBounds(50,75,200,30);
-        button3.setBounds(50,100,200,30);
-        button4.setBounds(50,125,200,30);
+        button0.setBounds(50,50,200,30);
+        button1.setBounds(50,75,200,30);
+        button2.setBounds(50,100,200,30);
+        button3.setBounds(50,125,200,30);
+        button4.setBounds(50,150,200,30);
 
         //Action listeners are used to get input from buttons
-        button1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                textfield.setText("Design 1");
-                setBackground(1);
-            }
+        button0.addActionListener(e -> {
+            textfield.setText("Design 0");
+            setBackground(0);
         });
-        button2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                textfield.setText("Design 2");
-                setBackground(2);
-            }
+        button1.addActionListener(e -> {
+            textfield.setText("Design 1");
+            setBackground(1);
         });
-        button3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                textfield.setText("Design 3");
-                setBackground(3);
-            }
+        button2.addActionListener(e -> {
+            textfield.setText("Design 2");
+            setBackground(2);
         });
-        button4.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                textfield.setText("Exit");
-                System.exit(0);
-            }
+        button3.addActionListener(e -> {
+            textfield.setText("Design 3");
+            setBackground(3);
+        });
+        button4.addActionListener(e -> {
+            textfield.setText("Exit");
+            System.exit(0);
         });
 
         //Add buttons to window
+        buttonWindow.add(button0);
         buttonWindow.add(button1);
         buttonWindow.add(button2);
         buttonWindow.add(button3);
         buttonWindow.add(button4);
 
         buttonWindow.add(textfield);
-        buttonWindow.setBounds(600,100,300,300);
+        buttonWindow.setBounds(605,100,300,300);
         buttonWindow.setLayout(null);
         buttonWindow.setVisible(true);
 
+    }
+
+    /*
+     * Create a window with clock
+     */
+    public void createClockWindow(){
         //The second JFrame object is used to depict the clock.
         JFrame windowClock = new JFrame();
 
@@ -212,15 +217,21 @@ public class AnalogClock extends JPanel implements Runnable
         windowClock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Window dimensions
-        windowClock.setBounds(0, 0, 600, 600);
+        windowClock.setBounds(0, 0, 615, 635);
 
         //Depict clock on window
-        windowClock.getContentPane().add(clock);
+        windowClock.getContentPane().add(this);
 
         //Set visibility
         windowClock.setVisible(true);
+    }
 
-        //Start clock procedure
-        clock.start();
+
+    public static void main(String[] args)
+    {
+        AnalogClock clock = new AnalogClock();    //Create instance of the class AnalogClock
+        clock.createButtons();                    //Create button window
+        clock.createClockWindow();                //Create clock window
+        clock.start();                            //Start clock procedure
     }
 }
